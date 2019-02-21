ab2682cc44f0f2011829383a24c7f35933d59bf6
xiaodongyan <east_yxd@163.com>
2019-02-21 17:41:23 +0800

    增加了一个自定义的log模块

--- SnrksMonitor/webspider.py
+++ SnrksMonitor/webspider.py
@@ -2,20 +2,22 @@
 east
 """
 
-
 import yaml
-import random
-from lxml import html
 import requests
+from lxml import html
+from SnrksMonitor.log import Logger
+
 
 # create a static dict to save history data
-history = []
+
+log = Logger().log()
 
 
 class WebSpider:
 
     def __init__(self):
         self.datadict = []
+        self.history = []
 
     @staticmethod
     def readyaml():
@@ -26,39 +28,43 @@ class WebSpider:
             global configdata
             configdata = yaml.load(f)
         except IOError:
-            # logging.log('open config failed')
-            print('open config failed')
+            # print('open config failed')
+            log.error('open config failed')
         return configdata
 
-    def download_imgage(self,url,fileurl):
-        print ('下载图片：%s'%url)
+    def download_imgage(self, url, fileurl):
+        log.info('start download image：%s' % url)
         r = requests.get(url=url)
-        with open(fileurl,'wb') as f:
+        with open(fileurl, 'wb') as f:
             f.write(r.content)
-        print('图片保存地址为：%s'%fileurl)
+        # print('图片保存地址为：%s' % fileurl)
+        log.info('the image save in：%s' % fileurl)
         f.close()
 
-    def spider(self,url,useragent,timeout):
+    def spider(self, url, useragent, timeout):
         # 爬取snrks网站内容
         # config = self.readyaml()
         # url = config['url']
         # useragent = random.choice(config['User_Agents'])
         header = {
-            'User_Agents':useragent
+            'User_Agents': useragent
         }
         # logging.log('start spiders')
-        print('开始请求nike网站')
-        r = requests.get(url=url, headers=header,timeout=timeout)
+        # print('开始请求nike网站')
+        log.info('start connect to nike')
+        r = requests.get(url=url, headers=header, timeout=timeout)
         etree = html.etree
         s = etree.HTML(r.text)
         # 以下为对nike网站的分析
+        log.info("start analysis nike'website")
         shoes_div = s.xpath('//figure[@class="d-md-h ncss-col-sm-12 va-sm-t pb0-sm prl0-sm"]')
         fileindex = 0
         for shoes in shoes_div:
+            self.datadict = []
             shoes_name = shoes.xpath('.//h3[@class="ncss-brand u-uppercase mb-1-sm fs16-sm"]/text()')[1]
             shoes_img = shoes.xpath('.//img/@src')
-            fileurl = './img/shoes%s.jpg'%fileindex
-            self.download_imgage(url=shoes_img[0],fileurl=fileurl)
+            fileurl = './img/shoes%s.jpg' % fileindex
+            self.download_imgage(url=shoes_img[0], fileurl=fileurl)
             shoes_time = shoes.xpath('.//h6//div/text()')
             shoes_dict = {}
             shoes_dict.update({
@@ -70,25 +76,23 @@ class WebSpider:
             self.datadict.append(shoes_dict)
             fileindex += 1
 
-    def data_analysis(self):
+    def data_analysis(self, update):
         # data check
-        update = []
-        print('开始记录是否有更新')
-        if history is None:
+        # print('开始记录是否有更新')
+        log.info('start checking whether updated or not')
+        if len(self.history) == 0:
             for shoes in self.datadict:
-                history.append(shoes)
-        elif history is not None:
+                self.history.append(shoes)
+        elif len(self.history) > 0:
             for shoes in self.datadict:
-                if shoes in history:
+                if shoes in self.history:
                     pass
-                elif shoes not in history:
+                elif shoes not in self.history:
                     update.append(shoes)
-        print('更新内容数量为%s'%len(update))
+        log.info('the number of updated:%s' % len(update))
         return update
-
 
 # if __name__ == '__main__':
 #     run = WebSpider()
 #     run.spider()
 #     print(run.data_analysis())
-
--- SnrksMonitor/wechatnotice.py
+++ SnrksMonitor/wechatnotice.py
@@ -2,58 +2,60 @@
 east
 """
 import itchat
-import time
-import yaml
 import os
+from SnrksMonitor.log import Logger
+
+log = Logger().log()
+
 
 class wechat():
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def login(self):
-		itchat.auto_login(hotReload=True)
+    def login(self):
+        itchat.auto_login(hotReload=True)
 
-	def getFriends(self):
-		friends = itchat.get_friends()
-		print(friends)
-		return friends
+    def getFriends(self):
+        friends = itchat.get_friends()
+        log.debug(friends)
+        return friends
 
-	def sendMessage(self,msg,user):
-		# 在群聊中发送推送并且删除图片
-		for item in msg:
-			print('开始发送图片')
-			message ='国家[%s] %s %s'%(item['country'],item['name'],item['time'])
-			itchat.send_msg(msg=message,toUserName=user)
-			itchat.send_image(fileDir=item['img'],toUserName=user)
-			try:
-				print('删除图片%s'%item['img'])
-				os.remove(path=item['img'])
-			except IOError:
-				print('删除失败')
-		print('推送结束，进入睡眠')
+    def sendMessage(self, msg, user):
+        # 在群聊中发送推送并且删除图片
+        for item in msg:
+            log.info('start sending image')
+            message = '国家[%s] %s %s' % (item['country'], item['name'], item['time'])
+            itchat.send_msg(msg=message, toUserName=user)
+            itchat.send_image(fileDir=item['img'], toUserName=user)
+            try:
+                log.info('delete image:%s' % item['img'])
+                os.remove(path=item['img'])
+            except IOError:
+                log.error('delete failed')
+        log.info('message has been send, waiting for next time to start')
 
-	def getChatRoomId(self,nickname):
-		# 获取群聊的username
-		groupContent = itchat.get_chatrooms()
-		#print(groupContent)
-		chatroomid = ''
-		for item in groupContent:
-			if item['NickName'] == nickname:
-				chatroomid = item['UserName']
-		print('成功获取群聊“%s”的ID：%s'%(nickname,chatroomid))
-		return chatroomid
+    def getChatRoomId(self, nickname):
+        # 获取群聊的username
+        groupContent = itchat.get_chatrooms()
+        # log.debug(groupContent)
+        chatroomid = ''
+        for item in groupContent:
+            if item['NickName'] == nickname:
+                chatroomid = item['UserName']
+        print('成功获取群聊“%s”的ID：%s' % (nickname, chatroomid))
+        return chatroomid
 
-if __name__ == '__main__':
-	groupid = ''
-	msg = ''
-	try:
-		f = open('./config.yaml')
-		c = yaml.load(f)
-		groupid = c['chatroomid']
-	except IOError:
-		print('open config.yaml failed')
-	chat = wechat ()
-	chat.login()
-	chat.getGroup()
-	# groupid = chat.getGroup()
 
+# if __name__ == '__main__':
+#     groupid = ''
+#     msg = ''
+#     try:
+#         f = open('./config.yaml')
+#         c = yaml.load(f)
+#         groupid = c['chatroomid']
+#     except IOError:
+#         print('open config.yaml failed')
+#     chat = wechat()
+#     chat.login()
+#     chat.getGroup()
+# # groupid = chat.getGroup()
